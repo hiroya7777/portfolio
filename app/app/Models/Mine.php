6ca37cf67b1b93db2ecc156d65c6b02394c9b55d
川口博哉 <kawaguchihiroya1117@gmail.com>
2020-04-28 12:53:16 +0900

    テーブルに対応するモデルを作成 #6

---
+++ app/app/Models/Mine.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Mine extends Model
+{
+    /**
+     * モデルと関連しているテーブル
+     *
+     * @var string
+     */
+    protected $table = 'mines';
+}
---
+++ app/app/Models/Stock.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Stock extends Model
+{
+    /**
+     * モデルと関連しているテーブル
+     *
+     * @var string
+     */
+    protected $table = 'stocks';
+}
