fb47b2e3fd2111ecaa370904815a6d5a4d721382
川口博哉 <kawaguchihiroya1117@gmail.com>
2020-04-28 12:31:11 +0900

    mineテーブルを作成 #6

---
+++ app/database/migrations/2020_04_28_025656_create_mines_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mines', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->comment('人名');
+            $table->integer('age')->comment('年齢');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mines');
+    }
+}
