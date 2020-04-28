2769f5a56d04012643c9d5542938300853a07cec
川口博哉 <kawaguchihiroya1117@gmail.com>
2020-04-28 14:55:05 +0900

    カート・トップ画面を作成 #8

--- app/resources/views/mycart.blade.php
+++ app/resources/views/mycart.blade.php
@@ -4,10 +4,10 @@
 <div class="container-fluid">
     <div class="">
         <div class="mx-auto" style="max-width:1200px">
-            <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">商品一覧</h1>
+            <h1 style="color:#555555; text-align:center; font-size:1.2em; padding:24px 0px; font-weight:bold;">カート一覧</h1>
             <div class="">
                 <div class="d-flex flex-row flex-wrap">
-                    商品一覧を出したい
+                    カート覧を出したい
 
                 {{-- 追加 --}}
 
