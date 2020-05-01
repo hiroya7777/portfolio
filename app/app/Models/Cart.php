a11ddadfc27c63f95b571bb62bb05add6a6f0872
川口博哉 <kawaguchihiroya1117@gmail.com>
2020-05-01 11:03:29 +0900

    crud処理で必要なメソッドを追加 #8

--- app/app/Models/Cart.php
+++ app/app/Models/Cart.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Cart extends Model
 {
+    use SoftDeletes;
+
     /**
      * モデルと関連しているテーブル
      *
@@ -15,6 +19,44 @@ class Cart extends Model
 
     protected $fillable = [
         'stock_id',
-        'cart_id'
+        'user_id'
     ];
+
+    public function showCart()
+    {
+        $userId = Auth::id();
+        return $this->where('user_id', $userId)->get();
+    }
+
+    public function stock()
+    {
+        return $this->belongsTo('\App\Models\Stock');
+    }
+
+    public function addCart($stockId)
+    {
+        $userId = Auth::id();
+        $cartInfo = Cart::firstOrCreate(['stock_id' => $stockId, 'user_id' => $userId]);
+
+        if($cartInfo->wasRecentlyCreated){
+            $message = 'カートに追加しました';
+        }
+        else{
+            $message = 'カートに登録済みです';
+        }
+        return $message;
+    }
+
+    public function deleteCart($stockId)
+    {
+        $userId = Auth::id();
+        $delete = $this->where('user_id', $userId)->where('stock_id',$stockId)->delete();
+
+        if($delete > 0){
+            $message = 'カートから一つの商品を削除しました';
+        }else{
+            $message = '削除に失敗しました';
+        }
+        return $message;
+    }
 }
