00667c633e345115f426df5373837bb075dfbfa2
川口博哉 <kawaguchihiroya1117@gmail.com>
2020-05-01 11:04:01 +0900

    一覧画面とカート画面を更新 #8

--- app/resources/views/mycart.blade.php
+++ app/resources/views/mycart.blade.php
@@ -2,27 +2,33 @@
 
 @section('content')
 <div class="container-fluid">
-   <div class="">
-       <div class="mx-auto" style="max-width:1200px">
-           <h1 class="text-center font-weight-bold" style="color:#555555;  font-size:1.2em; padding:24px 0px;">
-           {{ Auth::user()->name }}さんのカートの中身</h1>
+    <div class="">
+        <div class="mx-auto" style="max-width:1200px">
+            <h1 class="text-center font-weight-bold" style="color:#555555;  font-size:1.2em; padding:24px 0px;">
+            {{ Auth::user()->name }}さんのカートの中身</h1>
 
-           <div class="">
-               <p class="text-center">{{ $message ?? '' }}</p><br>
-               <div class="d-flex flex-row flex-wrap">
+            <div class="">
+                <p class="text-center">{{ $message ?? '' }}</p><br>
+                <div class="d-flex flex-row flex-wrap">
 
-                   @foreach($myCarts as $myCart)
-                       <div class="mycart_box">
-                           <p>ユーザーID：{{$myCart->user_id}}</p>
-                           <p>ストックID：{{$myCart->stock_id}}</p>
-                       </div>
-                   @endforeach
+                @foreach($myCarts as $myCart)
+                    <div class="mycart_box">
+                        {{ $myCart->stock->name }} <br>
+                        {{ number_format($myCart->stock->fee) }}円 <br>
+                        <img src="/image/{{$myCart->stock->imgpath}}" alt="" class="incart" >
+                        <br>
 
-               </div>
-
-               <a href="/">商品一覧へ</a>
-           </div>
-       </div>
-   </div>
+                        <form action="/cartdelete" method="post">
+                            @csrf
+                            <input type="hidden" name="stock_id" value="{{ $myCart->stock->id }}">
+                            <input type="submit" value="カートから削除する">
+                        </form>
+                    </div>
+                @endforeach
+            </div>
+                <a href="/">商品一覧へ</a>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
