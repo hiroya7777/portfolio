31502a54c62a7c4c8e62ace63fd47977524eddb5
川口博哉 <kawaguchihiroya1117@gmail.com>
2020-05-03 10:58:56 +0900

    twitter認証のルートを追加 #11

--- app/routes/web.php
+++ app/routes/web.php
@@ -16,4 +16,8 @@ Route::get('/mycart', 'ShopController@myCart');
 Route::post('/mycart', 'ShopController@addMycart');
 Route::post('/cartdelete', 'ShopController@deleteCart');
 
+// Twitter認証
+Route::get('login/twitter', 'Auth\LoginController@redirectToTwitterProvider');
+Route::get('login/twitter/callback', 'Auth\LoginController@handleTwitterProviderCallback');
+
 Auth::routes();
